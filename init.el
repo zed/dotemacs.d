8030a07d6c55c14ebfca6e9d4613b3c433eb7499
zed <isidore.john.r@gmail.com>
2017-08-09 16:44:59 +0300

    hydra-next-error

--- init.el
+++ init.el
@@ -51,6 +51,24 @@
     ("-" text-scale-decrease "out"))
 
   ;; Movement
+  ;; https://github.com/abo-abo/hydra/wiki/Compilation
+  (defhydra hydra-next-error
+    (global-map "C-x")
+    "
+Compilation errors:
+_j_: next error        _h_: first error    _q_uit
+_k_: previous error    _l_: last error
+"
+    ("`" next-error     nil)
+    ("j" next-error     nil :bind nil)
+    ("k" previous-error nil :bind nil)
+    ("h" first-error    nil :bind nil)
+    ("l" (condition-case err
+             (while t
+               (next-error))
+           (user-error nil))
+     nil :bind nil)
+    ("q" nil            nil :color blue))
   (global-set-key (kbd "C-n")  (defhydra hydra-move
      (:body-pre (forward-line))
      "move"
