506872348cff08bc7a5b9f2402549a8220336f27
zed <isidore.john.r@gmail.com>
2018-07-04 07:44:21 +0300

    add WAIT org keyword

--- init.el
+++ init.el
@@ -972,8 +972,7 @@ _q_ cancel     _D_lt Other      _S_wap           _m_aximize
 
 
   (setq org-todo-keywords
-	'((sequence "TODO(t)" "|" "DONE(d)")
-	  (sequence "|" "CANCELED(c)")))
+	'((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
 
   (setq org-tag-alist '((:startgroup . nil) ("home" . ?h) ("work" . ?w) (:endgroup . nil)
 			("idea" . nil) ("code" . nil) ("pythonista" . nil) ("org" . nil)
