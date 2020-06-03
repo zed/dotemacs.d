544ac87d4189eb8f3ca07985702905f922e967ce
zed <isidore.john.r@gmail.com>
2020-06-03 10:57:26 +0300

    Org: properties such as attachment dirs apply also for sublevels

--- init.el
+++ init.el
@@ -1179,6 +1179,7 @@ _q_ cancel     _D_lt Other      _S_wap           _m_aximize
 (use-package org
   :ensure nil
   :custom
+  (org-use-property-inheritance t "properties such as attachment dirs apply also for sublevels")
   (org-src-preserve-indenhtation nil "leading whitespace blocks are stripped")
   (org-edit-src-content-indentation 0 " and the code block is not indented (0!)")
   (org-src-tab-acts-natively t "make TAB insert spaces in python src blocks")
