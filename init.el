d3002cea26c5f37b759311c0c28cdcd0b2a78cb9
zed <isidore.john.r@gmail.com>
2021-12-01 07:54:34 +0300

    feat(org-babel): run code blocks in org-attach directory

--- init.el
+++ init.el
@@ -1096,6 +1096,28 @@ _q_ cancel     _D_lt Other      _S_wap           _m_aximize
 	  (:session . "py")
 	  (:kernel . "python3"))))
 
+;; https://www.mail-archive.com/emacs-orgmode@gnu.org/msg129554.html
+(define-advice org-babel-execute-src-block (:filter-args (&optional args)
+set-detault-dir-to-org-attach-path)
+  "Set working directory to the current entry's attach directory."
+  (if (eq major-mode 'org-mode)
+      (let* ((directory (file-name-as-directory (org-attach-dir
+'create-if-none)))
+             (arg (car args))
+             (info (cadr args))
+             (params (org-babel-merge-params (nth 2 info) (caddr args)))
+             (dir-param (alist-get :dir params)))
+        (unless (and dir-param (or (equal (f-full default-directory) (f-full
+dir-param))
+                                   (f-absolute-p dir-param)))
+          (setf (alist-get :dir params)
+                (if dir-param
+                    (f-join directory (alist-get :dir params))
+                  directory)))
+        (list arg info params))
+    (list arg info params)))
+
+
 ;; *** vi-like speed keys for org mode
 (use-package worf
   :bind (:map worf-mode-map
