38a1688bad003995696372a609c635f0689c9ea9
zed <isidore.john.r@gmail.com>
2024-08-14 13:22:47 +0300

    feat: use leuven theme
    - better highlighting behavior
    - colors are distracting -- let's see whether I can get used to it.

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
     (mapc 'disable-theme custom-enabled-themes))
   (advice-add 'load-theme :before #'init:disable-before-load)
   :config
-  (change-theme 'tango 'tango-dark))
+  (change-theme 'leuven 'leuven-dark))
 
 ;; ** navigating,searching,selecting lists ivy, swiper, counsel
 ;; *** ivy
